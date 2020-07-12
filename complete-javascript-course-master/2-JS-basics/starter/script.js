eb2d898a9c9f7d281c659965e484845345d2f316
5d5ng <deo1915@gmail.com>
2020-07-12 22:24:03 +0900

    first commit

---
+++ complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -0,0 +1,42 @@
+// var firstName = 'Dave'
+
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job); // 할당해주지않으면 Undefined 로 출력된다.
+
+// job = 'Teacher'
+// console.log(job);
+
+// var 3years = 3;
+
+// var firstName = 'John';
+// var age = 28;
+// console.log(firstName+" "+age);
+
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
+
+// alert(firstName+' is a ' + age+' year old '+job
+// + '. Is he married? '+isMarried);
+
+// var lastName = prompt('What is his last Name?'); //input시 사용
+// console.log(firstName +' '+lastName);
+
+//Basic operator
+var year,yearJohn,yearMark
+year = 2018;
+yearJohn = 2018 - 28;
+yearMark = 2018 - 33;
+console.log(yearJohn);
+
+var a = 10
+var b = 20
+var c = a <b
