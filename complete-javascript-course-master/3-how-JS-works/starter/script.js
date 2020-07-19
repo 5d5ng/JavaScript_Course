969e210e1bb8c41952210b53dd8cfc1e65fe81b6
5d5ng <deo1915@gmail.com>
2020-07-19 23:05:13 +0900

    how to work jsCode in background

--- complete-javascript-course-master/3-how-JS-works/starter/script.js
+++ complete-javascript-course-master/3-how-JS-works/starter/script.js
@@ -1,5 +1,33 @@
-///////////////////////////////////////
 // Lecture: Hoisting
+///////////////////////////////////////
+
+calcAge(1975);
+//함수를 정의해주기 전에도 함수가 잘 실행된다.
+//호이스팅
+
+//functions
+function calcAge(year){
+    console.log(2020-year);
+}
+
+// retirement(1999);
+
+var retirement = function(year){
+    console.log(65-(2016-year));
+}
+
+
+//variables
+console.log(age); //뒤에 선언한 경우 undefined 만약 출력코드 뒤에 라도 선언하지 않는다면 에러발생.
+var age =23;  //global execution context object에 저장된다.
+console.log(age); //23출력
+
+function foo(){
+    var age = 65;// 이 변수는 variable object 내부에 정의되었다.
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
